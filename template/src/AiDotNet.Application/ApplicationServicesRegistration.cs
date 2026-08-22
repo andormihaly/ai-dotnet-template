1d53e9a38a51097c5a0fa72647f6abb3b000e5d3
Andor Mihaly <andor_mihaly@yahoo.com>
2026-08-22 22:04:14 +0300

    Initial AI-Ready .NET template with Clean Architecture

---
+++ template/src/AiDotNet.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AiDotNet.Application;
+
+public static class ApplicationServicesRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
